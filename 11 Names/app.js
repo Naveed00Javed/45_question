33b7be985502a33a8aca42395085773fb83b9434
Naveed00Javed <njawaid87@gmail.com>
2024-06-06 09:48:57 -0700

    adding 11 names

---
+++ 11 Names/app.js
@@ -0,0 +1,11 @@
+// Ex 11
+// let friends: string[] = [`Zohaib`,`Arsalan`,`Saqib`,`Temoor`];
+// for(let i=0; i<friends.length; i++){
+//     console.log(friends[i]);
+// }
+// Ex 12
+var friends = ["Zohaib", "Arsalan", "Saqib", "Temoor"];
+var message = "You are invited a dinner for tomorrow ";
+for (var i = 0; i < friends.length; i++) {
+    console.log(message + friends[i]);
+}
---
+++ 11 Names/app.ts
@@ -0,0 +1,13 @@
+// Ex 11
+// let friends: string[] = [`Zohaib`,`Arsalan`,`Saqib`,`Temoor`];
+// for(let i=0; i<friends.length; i++){
+//     console.log(friends[i]);
+// }
+
+// Ex 12
+
+let friends: string[] = [`Zohaib`,`Arsalan`,`Saqib`,`Temoor`];
+let message: string =`You are invited a dinner for tomorrow `;
+ for(let i=0; i<friends.length; i++){
+    console.log(message + friends[i]);
+ }
